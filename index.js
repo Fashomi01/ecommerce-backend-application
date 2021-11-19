ab9d0305c0178296b99b8b60556d723bf64e1ac5
fashomi emmanuel <fashomi01@gmail.com>
2021-11-19 16:53:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//routers import
+const userRoutes = require("./routes/user");
+const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+
+//Middleware
+app.use(express.json());
+
+//routers
+app.use('/user', userRoutes);
+app.use('/product', productRoutes);
+app.use('/cart', cartRoutes);
+
+mongoose.connect('mongodb://localhost/ecommerce', () => console.log('Connected to db'))
+
+app.listen(port, () => {
+    console.log("The server is running now!");
+  });
---
+++ routes/verifyToken.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) return res.status(401).send('Access Denied');
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).send('Invalid Token');
+    }
+}
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user.id === req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("You are not alowed to do that!");
+      }
+    });
+};
+  
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("You are not alowed to do that!");
+      }
+    });
+};
+
+module.exports = {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
+}
